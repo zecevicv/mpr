d5a517c8e67df01a3870e6fe054083f02b18cfb8
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2021-11-06 20:06:07 +0100

    [ADD] - Homepage

---
+++ js/app.js
@@ -0,0 +1,43 @@
+/* #Header
+  ======================================================= */
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const hamburgerBtn = document.querySelector('.header-hamburger');
+
+hamburgerBtn.addEventListener('click', (e) => {
+  body.classList.toggle('no-scroll');
+  header.classList.toggle('show');
+});
+
+/* #Banner
+  ======================================================= */
+if (document.querySelector('.home-page .banner .swiper')) {
+  new Swiper(".home-page .banner .swiper", {
+    loop: true,
+    navigation: {
+      nextEl: ".home-page .banner .next",
+      prevEl: ".home-page .banner .prev",
+    },
+  });
+}
+
+/* #Home About 2
+  ======================================================= */
+if (document.querySelector('.home-about-2 .swiper')) {
+  new Swiper(".home-about-2 .swiper", {
+    centeredSlides: true,
+    loop: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1.5
+      },
+      1024: {
+        slidesPerView: 3,
+      }
+    },
+    navigation: {
+      nextEl: ".home-about-2 .next",
+      prevEl: ".home-about-2 .prev",
+    },
+  });
+}
